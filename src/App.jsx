dd3f3cecf498cbae33fc7ac34bf2222691dccb6a
Mintu Singh <mintusingh2006j@gmail.com>
2025-11-11 09:13:31 +0530

    pin page setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import CheckBalance from './pages/CheckBalance';
+import Pin from './pages/Pin';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/check-balance' element={<CheckBalance />} />
+        <Route path='/pin' element={<Pin />} />
       </Routes>
     </Router>
   );
--- src/pages/CheckBalance.jsx
+++ src/pages/CheckBalance.jsx
@@ -3,6 +3,8 @@ import styled from 'styled-components'
 import { IoArrowBackOutline } from "react-icons/io5";
 import { GoQuestion } from "react-icons/go";
 import { IoIosArrowForward } from "react-icons/io";
+import { CiWallet } from "react-icons/ci";
+import { useNavigate } from 'react-router-dom';
 
 const Container = styled.div`
   height: 100vh;
@@ -28,25 +30,50 @@ const Box = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-`
+`;
+
 const CheckBalance = () => {
+  const navigate = useNavigate();
+
+  // 🔹 Function to handle navigation
+  const handleBackHome = () => {
+    navigate('/');
+  };
+  const handlePinPage = () => {
+    navigate('/pin');
+  };
+
   return (
     <Container>
       <Header>
-        <IoArrowBackOutline style={{ fontSize: "3vh", fontWeight: 200 }} />
+        <IoArrowBackOutline style={{ fontSize: "3vh", fontWeight: 200 }} onClick={handleBackHome}/>
         <GoQuestion style={{ fontSize: "3vh", fontWeight: 200 }} />
       </Header>
-      <h1 style={{margin: "3vh 0 4vh 0", fontSize: "2.8vh", color: "#dedede"}}>Check Balance</h1>
+      <h1 style={{ margin: "3vh 0 4vh 0", fontSize: "2.8vh", color: "#dedede" }}>Check Balance</h1>
       <BankBox>
-        <Box>
-          <div style={{display: "flex", alignItems: 'center'}}>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS51-yXH33Jr7d3xc_qdspjImvqAiXFvhPOdQ&s" alt="bank logo" style={{height: "5vh", borderRadius: "1.8vh", marginRight: "2vh"}}/>
-            <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', lineHeight: "2.6vh"}}>
-              <p style={{fontSize: "1.8vh"}}>State Bank of India - 0428</p>
-              <p style={{fontSize: "1.5vh", color: "#a1a1a1"}}>Bank Account</p>
+
+        {/* MAIN BANK */}
+        <Box onClick={handlePinPage}>
+          <div style={{ display: "flex", alignItems: 'center' }}>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS51-yXH33Jr7d3xc_qdspjImvqAiXFvhPOdQ&s" alt="bank logo" style={{ height: "5vh", borderRadius: "1.8vh", marginRight: "2vh" }} />
+            <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', lineHeight: "2.6vh" }}>
+              <p style={{ fontSize: "1.8vh" }}>State Bank of India - 0428</p>
+              <p style={{ fontSize: "1.5vh", color: "#a1a1a1" }}>Bank Account</p>
             </div>
           </div>
-          <IoIosArrowForward style={{color: "#a1a1a1"}}/>
+          <IoIosArrowForward style={{ color: "#a1a1a1" }} />
+        </Box>
+
+        {/* PHONEPE WALLET */}
+        <Box>
+          <div style={{ display: "flex", alignItems: 'center' }}>
+            <CiWallet style={{ fontSize: "5vh", marginRight: "2vh", fontWeight: 200 }} />
+            <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', lineHeight: "2.6vh" }}>
+              <p style={{ fontSize: "1.8vh" }}>PhonePe Wallet</p>
+              <p style={{ fontSize: "1.5vh", color: "#a1a1a1" }}>Balance: ₹0</p>
+            </div>
+          </div>
+          <p style={{ color: "purple", fontSize: "1.6vh", fontWeight: 600 }}>Activate</p>
         </Box>
       </BankBox>
     </Container>
